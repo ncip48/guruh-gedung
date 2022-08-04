9d76ea36dc9e2a130befeb1dbb3a2954f3536976
ncip48 <mbahcip00@gmail.com>
2022-08-04 10:36:21 +0000

    asu

---
+++ app/Http/Controllers/ReservasiController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gedung;
+use App\Models\Reservasi;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ReservasiController extends Controller
+{
+    //check a vailability of a table by time and date
+    public function cekGedung(Request $request)
+    {
+        //mendapatkan tanggal dari request json / ajax
+        $tanggal = $request->tanggal;
+        //mendapatkan id_gedung dari request json / ajax
+        $id_gedung = $request->id_gedung;
+        //validasi euy
+        if ($tanggal == "NaN-NaN-NaN" && !$id_gedung) {
+            return ResponseController::response(false, '', null);
+        }
+        if ($tanggal == "NaN-NaN-NaN") {
+            return ResponseController::response(false, 'Mohon isi tanggal dahulu', null);
+        }
+        if (!$id_gedung) {
+            return ResponseController::response(false, 'Mohon pilih gedung dahulu', null);
+        }
+
+        $cek_tersedia = Reservasi::where('tanggal', $tanggal)->where('id_gedung', $id_gedung)->count();
+
+        if ($cek_tersedia == 1) {
+            return ResponseController::response(false, 'Gedung tidak tersedia', null);
+        } else {
+            return ResponseController::response(true, 'Gedung tersedia', null);
+        }
+    }
+
+    public function index()
+    {
+        //get gedung all ~> (SELECT * FROM gedung)
+        $gedung = Gedung::all();
+        //ini di file resources/views/home.blade.php
+        //compact tuh artine ngebawa parameter contone parameter $gedung dilempar ke view home.blade.php
+        //jadi nanti di home.blade.php kita bisa mengambil data gedung dari $gedung
+        return view('home', compact('gedung'));
+    }
+
+    public function search(Request $request)
+    {
+        $date = $request->date;
+        $gedung = Gedung::all();
+        $tersedia = [];
+        foreach ($gedung as $ged) {
+            $cek_tersedia = Reservasi::where('tanggal', $date)->where('id_gedung', $ged->id)->count();
+            $ged->tersedia = $cek_tersedia == 1 ? false : true;
+            $tersedia[] = $ged;
+        }
+        // return ResponseController::response(true, 'Sukses', $tersedia);
+        return view('cari', compact('tersedia', 'date'));
+    }
+}
---
+++ database/migrations/2022_07_04_093544_create_reservasi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservasi', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->integer('id_user')->nullable()->default(0);
+            $table->integer('id_gedung');
+            $table->string('total');
+            $table->date('tanggal');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservasi');
+    }
+};
