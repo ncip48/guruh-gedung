df45be691f232320f05d0d8b66e4586d3b39f29f
ncip48 <mbahcip00@gmail.com>
2022-08-09 03:12:08 +0000

    f

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,29 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {   
+        $input = $request->all();
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ],[
+            'email.required' => 'Email harus diisi',
+            'email.email' => 'Format email salah',
+            'password.required' => 'Password harus diisi'
+        ]);
+
+        if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password'])))
+        {
+            if (auth()->user()->role == 1) {
+                return redirect()->route('admin.home');
+            }else{
+                return redirect()->route('home');
+            }
+        }else{
+            return redirect()->route('login')
+                ->with('error','Email-Address And Password Are Wrong.');
+        }
+    }
 }
