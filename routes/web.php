7bf3da464a9403a17bd656129542d0b1de51288d
ncip48 <mbahcip00@gmail.com>
2022-08-09 03:09:34 +0000

    auth

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CallbackController;
 use App\Http\Controllers\GaleriController;
 use App\Http\Controllers\ReservasiController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,10 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ReservasiController::class, 'index']);
+Route::get('/', [ReservasiController::class, 'index'])->name('home');
 
 Route::get('/reservasi', [ReservasiController::class, 'reservasi']);
 Route::get('/cari', [ReservasiController::class, 'search']);
 Route::post('/booking', [ReservasiController::class, 'booking']);
 Route::get('/order', [ReservasiController::class, 'order']);
 Route::get('/gallery', [GaleriController::class, 'index']);
+
+Auth::routes();
+
+Route::get('admin/home', [HomeController::class, 'index'])->name('admin.home')->middleware('is_admin');
