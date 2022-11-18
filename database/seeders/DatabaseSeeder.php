7c16512de6915da0e8b4875855f33c9793779090
ncip48 <mbahcip00@gmail.com>
2022-11-18 17:03:03 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             GedungSeeder::class,
             GaleriSeeder::class,
             SiteSeeder::class,
+            BankSeeder::class,
+            RekeningSeeder::class,
         ]);
     }
 }
