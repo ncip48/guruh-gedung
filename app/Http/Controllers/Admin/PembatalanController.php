f4cde3b363dd35b81bd718b53cf4b060e534140a
ncip48 <mbahcip00@gmail.com>
2023-01-06 20:10:52 +0700

    pembatalan wa

--- app/Http/Controllers/Admin/PembatalanController.php
+++ app/Http/Controllers/Admin/PembatalanController.php
@@ -99,6 +99,12 @@ class PembatalanController extends Controller
         Reservasi::where('id', $pembatalan->id_reservasi)->update([
             'status' => 3
         ]);
+        $reservasi = Reservasi::where('id', $pembatalan->id_reservasi)->first();
+        $pesan = 'Pembatalan reservasi dengan kode $_KODE_$ atas nama $_NAMA_$ telah disetujui, silahkan cek link berikut untuk melihat detail reservasi anda: $_LINK_$';
+        $pesan = str_replace('$_KODE_$', $reservasi->kode, $pesan);
+        $pesan = str_replace('$_LINK_$', url('reservasi/' . $reservasi->kode), $pesan);
+        $pesan = str_replace('$_NAMA_$', $reservasi->nama, $pesan);
+        ReservasiController::sendWhatsapp($reservasi->no_hp, $pesan);
         return redirect()->route('pembatalan.index')->with('success', 'Pembatalan Reservasi Berhasil Diterima');
     }
 
